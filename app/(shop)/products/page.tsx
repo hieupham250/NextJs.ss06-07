1693fd361de72854650331957ad1d520cf3dd95c
hieupham250 <ptrunghieu2507@gmail.com>
2024-08-20 09:30:48 +0700

    bài tap

--- app/(shop)/products/page.tsx
+++ app/(shop)/products/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 export default function Products() {
-  return <div>page Products</div>;
+  const searchParams: any = useSearchParams();
+  const name = searchParams.get("name");
+  const category = searchParams.get("category");
+
+  return (
+    <div>
+      <p>tên sản phẩm: {name}</p>
+      <p>danh mục: {category}</p>
+    </div>
+  );
 }
